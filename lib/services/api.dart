75162b0548b9b2e355614dd9f0c927b7ceef2602
EsioFreitas <esio.gustavo@gmail.com>
2021-02-14 10:51:00 -0300

    feat: add api application

---
+++ lib/services/api.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Api {
+  String token;
+  Api._privateConstructor();
+
+  static final Api _instance = Api._privateConstructor();
+
+  factory Api() {
+    return _instance;
+  }
+
+  Future<String> getToken() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    String token = localStorage.getString('token');
+    return token;
+  }
+
+  final String _url = "";
+
+  static final headers = {"content-type": 'application/json'};
+
+  Future<http.Response> get({@required String route}) async {
+    String token = await getToken();
+    var response = await http.get(_url + route, headers: {
+      ...headers,
+      "authorization": token,
+    });
+
+    if (response.statusCode != 200) {
+      throw response;
+    }
+
+    return response;
+  }
+
+  Future<http.Response> post({
+    @required String route,
+    @required dynamic body,
+  }) async {
+    String token = await getToken();
+
+    return http.post(_url + route, body: jsonEncode(body), headers: {
+      ...headers,
+      "authorization": token,
+    });
+  }
+
+  Future<http.Response> put({
+    @required String route,
+    @required dynamic body,
+  }) async {
+    String token = await getToken();
+
+    return http.put(_url + route, body: jsonEncode(body), headers: {
+      ...headers,
+      "authorization": token,
+    });
+  }
+
+  Future<http.Response> delete({@required String route}) async {
+    String token = await getToken();
+
+    return http.delete(_url + route, headers: {
+      ...headers,
+      "authorization": token,
+    });
+  }
+}
