77f7acfab34c98c0efe75b9098c9ad64e576873d
EsioFreitas <esio.gustavo@gmail.com>
2021-02-14 10:24:45 -0300

    feat: add home page

---
+++ lib/view/home/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Home'),
+    );
+  }
+}
