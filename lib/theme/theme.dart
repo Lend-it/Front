2ff220fe86e8a8a3b913352e82993a384dd041b4
EsioFreitas <esio.gustavo@gmail.com>
2021-02-14 10:19:39 -0300

    feat: add project theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final ThemeData projectTheme = _buildProjectTheme();
+
+ThemeData _buildProjectTheme() {
+  final ThemeData base = ThemeData.light();
+
+  return base.copyWith();
+}
