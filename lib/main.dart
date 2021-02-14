9cbc38eb0039f46aa4a66487900b7dd113815b4d
EsioFreitas <esio.gustavo@gmail.com>
2021-02-14 10:27:47 -0300

    feat: conect theme to application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front/theme/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      theme: projectTheme,
     );
   }
 }
