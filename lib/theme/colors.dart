ad058011c5c213eb690e69ae3093cb5ba4b0cd3a
EsioFreitas <esio.gustavo@gmail.com>
2021-03-06 21:12:33 -0300

    feat: add app colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
-final dangerColor = const Color(0xffE47171);
+final dangerColor = const Color(0xffD94747);
 final lightColor = const Color(0xfff7f7f7);
 final darkColor = const Color(0xff353535);
+final grayColor = const Color(0xff454545);
+final primaryColor = const Color(0xff005A7A);
+final primaryLightColor = const Color(0xff1B82A6);
+final secondaryColor = const Color(0xffFF8400);
+final secondaryLightColor = const Color(0xffFFB15E);
