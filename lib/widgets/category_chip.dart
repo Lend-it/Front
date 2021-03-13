60f007f058c8df0616bbbc424b25497b69c43ba0
Mateus Maia <mateusmaiamaia@hotmail.com>
2021-03-13 00:37:35 -0300

    refactor: fga-eps-mds/2020.2-Lend.it#88 change CategoryChip to an animated widget
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- lib/widgets/category_chip.dart
+++ lib/widgets/category_chip.dart
@@ -5,25 +5,35 @@ import 'package:front/theme/colors.dart';
 class CategoryChip extends StatelessWidget {
   final bool isSelected;
   final String label;
+  final Function onTapHandler;
 
   CategoryChip({
     @required this.label,
+    @required this.onTapHandler,
     this.isSelected = false,
   });
 
   @override
   Widget build(BuildContext context) {
     Color chipColor = isSelected ? secondaryColor : grayColor;
+    Duration animationDuration = Duration(milliseconds: 300);
 
-    return Chip(
-      backgroundColor: Colors.transparent,
-      label: Text(
-        label,
-        style: TextStyle(color: chipColor),
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(6),
-        side: BorderSide(color: chipColor),
+    return InkWell(
+      onTap: onTapHandler,
+      child: AnimatedContainer(
+        duration: animationDuration,
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        child: Center(
+          child: AnimatedDefaultTextStyle(
+            duration: animationDuration,
+            style: TextStyle(color: chipColor),
+            child: Text(label),
+          ),
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(color: chipColor),
+          borderRadius: BorderRadius.circular(6),
+        ),
       ),
     );
   }
