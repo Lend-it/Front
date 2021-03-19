12cd82e5e5de90506be58b1ed296a8bdc7d6f957
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-03-19 17:43:05 -0300

    refactor: fga-eps-mds/2020.2-Lend.it#97 Refactor base page, add header prop
    Co-authored-by: Mateus Maia <mateusmaiamaia@hotmail.com>
    Co-authored-by: Esio Freitas <esio.gustavo@gmail.com>

--- lib/widgets/base_page.dart
+++ lib/widgets/base_page.dart
@@ -4,9 +4,11 @@ import 'package:front/theme/colors.dart';
 
 class BasePage extends StatelessWidget {
   final Widget body;
+  final Widget header;
 
   BasePage({
     @required this.body,
+    this.header,
   });
 
   @override
@@ -17,43 +19,45 @@ class BasePage extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: primaryColor,
-      body: SingleChildScrollView(
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              top: statusBarHeight,
-              right: -26,
-              child: SvgPicture.asset(
-                'assets/logo_water_mark.svg',
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: statusBarHeight,
+            right: -26,
+            child: SvgPicture.asset(
+              'assets/logo_water_mark.svg',
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: size.height * 0.08,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                margin: EdgeInsets.only(top: 64, bottom: 25),
+                child: header != null
+                    ? header
+                    : SvgPicture.asset(
+                        'assets/logo.svg',
+                        width: 150,
+                      ),
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    margin: EdgeInsets.only(top: 64, bottom: 25),
-                    child: SvgPicture.asset(
-                      'assets/logo.svg',
-                      width: 150,
-                    ),
+              Container(
+                width: size.width,
+                height: size.height * 0.8,
+                decoration: BoxDecoration(
+                  color: lightColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
                   ),
                 ),
-                SizedBox(height: 30),
-                Container(
-                  width: size.width,
-                  height: size.height,
-                  decoration: BoxDecoration(
-                    color: lightColor,
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                child: body,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
