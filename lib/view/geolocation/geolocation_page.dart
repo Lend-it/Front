0de3b5e14bec45147d95fa64b4d5d61e55aa6f58
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-03-19 18:09:48 -0300

    feat: fga-eps-mds/2020.2-Lend.it#97 Finish geolocation page layout adding your body
    Co-authored-by: Mateus Maia <mateusmaiamaia@hotmail.com>

--- lib/view/geolocation/geolocation_page.dart
+++ lib/view/geolocation/geolocation_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:front/theme/colors.dart';
 import 'package:front/widgets/base_page.dart';
+import 'package:front/widgets/button.dart';
 
 class GeolocationPage extends StatefulWidget {
   @override
@@ -10,10 +13,35 @@ class GeolocationPage extends StatefulWidget {
 class _GeolocationPageState extends State<GeolocationPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BasePage(
-        body: Container(),
+    return BasePage(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 15),
+          SvgPicture.asset(
+            'assets/location.svg',
+            width: 400,
+          ),
+          Text(
+            "Precisamos saber onde mora!",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: darkColor,
+              fontSize: 24,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            "Usamos a localização para filtrar os pedidos próximos e avisar os seus vizinhos quando você pedir algo emprestado.",
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: darkColor,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 80),
+          Button(title: "Permitir localização", onPressedHandler: () {})
+        ],
       ),
     );
   }
