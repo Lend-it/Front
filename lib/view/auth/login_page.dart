d627cc47cb30a6bc8f3a56457209ce8a612c0d31
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-03-22 23:42:31 -0300

    feat: fga-eps-mds/2020.2-Lend.it#110 Finish login page
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

--- lib/view/auth/login_page.dart
+++ lib/view/auth/login_page.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:front/theme/colors.dart';
+import 'package:front/widgets/button.dart';
+import 'package:front/widgets/input.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _emailController;
+  TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final double statusBarHeight = MediaQuery.of(context).padding.top;
@@ -57,6 +81,85 @@ class LoginPage extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: primaryColor,
                     borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 40),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Bem vindo ao Lend.it",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: lightColor,
+                                fontSize: 24,
+                              ),
+                            ),
+                            SizedBox(height: 12),
+                            Text(
+                              "Insira seus dados para fazer o login e começar a ajudar seus vizinhos!",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: lightColor,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            Input(
+                              type: TextInputType.emailAddress,
+                              controller: _emailController,
+                              placeholder: 'E-mail',
+                              prefix: Icons.mail_outline,
+                            ),
+                            Input(
+                              type: TextInputType.visiblePassword,
+                              controller: _passwordController,
+                              placeholder: 'Senha',
+                              prefix: Icons.lock_outline,
+                              isPassword: true,
+                            ),
+                            Text(
+                              "Esqueceu sua senha? Recupere aqui",
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: lightColor,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 15),
+                            Button(
+                              title: "Entrar",
+                              onPressedHandler: () {},
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Divider(
+                        height: 45,
+                        thickness: 2,
+                      ),
+                      Center(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Padding(
+                            padding: EdgeInsets.only(bottom: 15),
+                            child: Text(
+                              'Não tem uma conta? Crie sua conta aqui',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: lightColor,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ],
