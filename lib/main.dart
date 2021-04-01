d55f35e97a6308f23de6bd6e3c50c274fc1c25ca
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-01 19:56:56 -0300

    feat: fga-eps-mds/2020.2-Lend.it create provider for session data
    Co-authored-by: Mateus Maia <mateusmaiamaia@hotmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:front/routes/routes.dart';
 import 'package:front/theme/theme.dart';
 import 'package:front/widgets/bottom_navigation.dart';
+import 'package:provider/provider.dart';
+
+import 'model/session.model.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,18 +14,25 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: projectTheme,
-      initialRoute: '/login',
-      home: BottomNavigation(),
-      onGenerateRoute: (settings) {
-        return CupertinoPageRoute(
-          builder: (context) => Routes.routes[settings.name](context),
-          settings: settings,
-        );
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => SessionModel(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: projectTheme,
+        initialRoute: '/login',
+        home: BottomNavigation(),
+        onGenerateRoute: (settings) {
+          return CupertinoPageRoute(
+            builder: (context) => Routes.routes[settings.name](context),
+            settings: settings,
+          );
+        },
+      ),
     );
   }
 }
