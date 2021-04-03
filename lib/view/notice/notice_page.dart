64efd48d3d61e8dbf5b7ed1e4dab18b099666498
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-03 01:02:39 -0300

    feat: fga-eps-mds/2020.2-Lend.it#123 finalize lend at notice page
    Co-authored-by: Mateus Maia <mateusmaiamaia@hotmail.com>

--- lib/view/notice/notice_page.dart
+++ lib/view/notice/notice_page.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:front/routes/app_routes.dart';
+import 'package:front/controller/lend.controller.dart';
+import 'package:front/model/lend.model.dart';
+import 'package:front/utils/confirm_modal.dart';
 
-class NoticePage extends StatelessWidget {
+class NoticePage extends StatefulWidget {
+  @override
+  _NoticePageState createState() => _NoticePageState();
+}
+
+class _NoticePageState extends State<NoticePage> {
+  LendController lendController = new LendController();
+  List<LendModel> lends = [];
+
+  void getAllLends() async {
+    final List<LendModel> response = await lendController.getLends();
+    setState(() {
+      lends = response;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getAllLends();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,13 +35,19 @@ class NoticePage extends StatelessWidget {
       body: Container(
         child: Center(
           child: FlatButton(
-            onPressed: () {
-              Navigator.pushNamed(
-                context,
-                AppRoutes.LOGIN_PAGE,
-              );
-            },
-            child: Text('Criar Lend'),
+            onPressed: () => ConfirmModal.showConfirmModal(
+              context: context,
+              lend: lends[4],
+              title: 'Confirmação de devolução.',
+              subtitle:
+                  'Você realmente quer devolver o ${lends[4].title} para ${lends[4].requester.name}?',
+              confirmButtonText: 'Sim, quero devolver',
+              declineButtonText: 'Não, quero voltar atrás',
+              confirmPressed: () async {
+                await lendController.finalizeLend(lends[4].id);
+              },
+            ),
+            child: Text('finalizar lend'),
             color: Colors.blue,
           ),
         ),
